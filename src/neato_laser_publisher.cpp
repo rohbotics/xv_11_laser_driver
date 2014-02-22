b35ea1c633426ee131d5eb950b2f51b1acb3537d
Rohan Agrawal <send2arohan@gmail.com>
2014-02-22 10:11:33 -0800

    Pulled in Steve 'dillo Okay's RPMs topic

--- src/neato_laser_publisher.cpp
+++ src/neato_laser_publisher.cpp
@@ -36,6 +36,7 @@
 #include <sensor_msgs/LaserScan.h>
 #include <boost/asio.hpp>
 #include <xv_11_laser_driver/xv11_laser.h>
+#include <std_msgs/UInt16.h>
 
 int main(int argc, char **argv)
 {
@@ -47,10 +48,12 @@ int main(int argc, char **argv)
   int baud_rate;
   std::string frame_id;
   int firmware_number;
+ 
+  std_msgs::UInt16 rpms; 
 
   priv_nh.param("port", port, std::string("/dev/ttyUSB0"));
   priv_nh.param("baud_rate", baud_rate, 115200);
-  priv_nh.param("frame_id", frame_id, std::string("/laser"));
+  priv_nh.param("frame_id", frame_id, std::string("neato_laser"));
   priv_nh.param("firmware_version", firmware_number, 1);
 
   boost::asio::io_service io;
@@ -58,13 +61,17 @@ int main(int argc, char **argv)
   try {
     xv_11_laser_driver::XV11Laser laser(port, baud_rate, firmware_number, io);
     ros::Publisher laser_pub = n.advertise<sensor_msgs::LaserScan>("scan", 1000);
+    ros::Publisher motor_pub = n.advertise<std_msgs::UInt16>("rpms",1000);
 
     while (ros::ok()) {
       sensor_msgs::LaserScan::Ptr scan(new sensor_msgs::LaserScan);
       scan->header.frame_id = frame_id;
       scan->header.stamp = ros::Time::now();
       laser.poll(scan);
+      rpms.data=laser.rpms;
       laser_pub.publish(scan);
+      motor_pub.publish(rpms);
+
     }
     laser.close();
     return 0;
