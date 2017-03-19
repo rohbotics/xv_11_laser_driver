4f4dd5c8c2d2aeef06046e0a7dbacfbb759a3a65
Rohan Agrawal <send2arohan@gmail.com>
2017-03-19 11:44:33 -0700

    update default firmware_version to 2

--- src/neato_laser_publisher.cpp
+++ src/neato_laser_publisher.cpp
@@ -54,7 +54,7 @@ int main(int argc, char **argv)
   priv_nh.param("port", port, std::string("/dev/ttyUSB0"));
   priv_nh.param("baud_rate", baud_rate, 115200);
   priv_nh.param("frame_id", frame_id, std::string("neato_laser"));
-  priv_nh.param("firmware_version", firmware_number, 1);
+  priv_nh.param("firmware_version", firmware_number, 2);
 
   boost::asio::io_service io;
 
